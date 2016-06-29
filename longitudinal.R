91fe226bc4f9ab9d9e7df6c679e23a2bcd81d31a
Siyang Zeng <ho20037900@gmail.com>
2016-06-29 13:21:44 -0700

    6/29/2016-2

--- longitudinal.R
+++ longitudinal.R
@@ -17,14 +17,19 @@ concussion[,4:33] = apply(concussion[,4:33],2, as.numeric)
 concussion$Date = as.Date(concussion$Date)
 concussion$Injury.date = as.Date(concussion$Injury.date)
 concussion = mutate(concussion, days_aft_injury = as.numeric(Date - Injury.date))
+concussion = mutate(concussion, week = ceiling(days_aft_injury/7))
 
 #Exploratory
 length(unique(concussion$ID))      #number of distinct subjects
 nrow(unique(concussion[,c(1,3)]))   #number of subject-injury records
 
 d1= dcast(concussion[,1:7], ID + `Injury.date` + Age + Gender + Race~Date)
-d_a1 = dcast(concussion[,c(1:3,10)], ID + `Injury.date` ~Date)
-d_dai = dcast(concussion[,c(1:3,34)], ID + `Injury.date` ~Date)
+#d_a1 = dcast(concussion[,c(1:3,10)], ID + `Injury.date` ~Date)
+my_mean = function(x){
+  res = mean(x, na.rm = T)
+}
+d_a1 = dcast(concussion[,c(1,3,35,10)], ID + `Injury.date` ~week, my_mean)
+plot(x=0:56,y=d_a1[82,3:59])
 
 concussion = as.matrix(concussion)
 
