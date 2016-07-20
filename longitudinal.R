b81aa6d6ec512ddb9f68f94fc5ffc775be9605cd
Siyang Zeng <ho20037900@gmail.com>
2016-07-20 14:24:23 -0700

    07202016

--- longitudinal.R
+++ longitudinal.R
@@ -7,11 +7,15 @@ library(reshape2)
 library(dplyr)
 #install.packages("mi)
 library(mi)
+library(gplots)
 
 ###Import
 setwd("C:/Longitudinal-Project")
 concussion <- read.csv("C:/Longitudinal-Project/concussion.csv", stringsAsFactors=FALSE)
 concussion = concussion[,1:33]
+
+traj_test <- read.csv("C:/Longitudinal-Project/traj_test.csv", stringsAsFactors=FALSE)
+
 
 ###Cleaning
 concussion[,4:33] = apply(concussion[,4:33],2, as.numeric)
@@ -19,6 +23,20 @@ concussion$Date = as.Date(concussion$Date)
 concussion$Injury.date = as.Date(concussion$Injury.date)
 concussion = mutate(concussion, days_aft_injury = as.numeric(Date - Injury.date))
 concussion = mutate(concussion, week = ceiling(days_aft_injury/7))
+
+sub3069 = traj_test[which(traj_test$ID==3069),]
+
+unique(traj_test$Age)
+sub_age3 = subset(traj_test,Age==3)
+sub_age6 = subset(traj_test,Age==6)
+sub_age9 = subset(traj_test,Age==9)
+sub_age12 = subset(traj_test,Age==12)
+sub_age18 = subset(traj_test,Age==18)
+sub_age24 = subset(traj_test,Age==24)
+sub_age36 = subset(traj_test,Age==36)
+
+indicator = ifelse(sub_age12$class=="asd", "red",ifelse(sub_age12$class=="typ","green","black"))
+heatmap(as.matrix(scale(sub_age12[,5:1030])),RowSideColors = indicator)
 
 #Exploratory
 length(unique(concussion$ID))      #number of distinct subjects
@@ -28,6 +46,12 @@ d1= dcast(concussion[,1:7], ID + `Injury.date` + Age + Gender + Race~Date)
 #d_a1 = dcast(concussion[,c(1:3,10)], ID + `Injury.date` ~Date)
 my_mean = function(x){
   res = mean(x, na.rm = T)
+}
+
+plot(x=sub3069$Age,y = sub3069$C3.Power.s0, type = "l")
+colors=rainbow(100)
+for(j in 6:1030){
+  points(x = sub3069$Age, y = sub3069[,j], type = "l",col = colors[j])
 }
 
 ##Missing data method 1
@@ -51,19 +75,35 @@ colnames(df4fit)=c("y","x")
 plot(y~x, data = df4fit, type="l")
 fit1 = lm(y~x, data = df4fit[1:5,])
 for(i in 1:nrow(d2_a1)){
-  na_index = which(is.na(d2_a1[i,]))
-  fit_index = i:i+4
-  fit = lm()
+  df4fit = rbind(d2_a1[i,3:59],x)
+  df4fit = data.frame(t(df4fit))
+  colnames(df4fit)=c("y","x")
+  na_index = which(is.na(df4fit))
+  for(j in 1:(nrow(df4fit)-4)){
+    fit_index = j:(j+4)
+    fit = lm(y~x,df4fit[fit_index,])
+    match_index = intersect(na_index,fit_index)
+    for(k in match_index){
+      if(!is.na(df4fit[k,1])){
+        df4fit[k,1]=(df4fit[k,1]+(fit$coef[1]+fit$coef[2]*df4fit[k,2]))/2
+      }
+      else{df4fit[k,1]=fit$coeff[1]+fit$coef[2]*df4fit[k,2]
+      }
+      
+    }
+    print(c(i,j))
+  }
+  d2_a1[i,3:59]=t(df4fit[,1])
 }
 
 
 test = missing_data.frame(d2_a1[,3:59])
 test = missing_data.frame(test)
 
-plot(x=0:56,y=d_a1[1,3:59],type="l",ylim=c(-1,7))
+plot(x=0:56,y=d2_a1[1,3:59],type="l",ylim=c(-1,7))
 colors=rainbow(199)
 for(i in 2:200){
-  points(x=0:56, y=d_a1[i,3:59],type="l", col=colors[i])
+  points(x=0:56, y=d2_a1[i,3:59],type="l", col=colors[i])
 }
 
 
@@ -72,11 +112,11 @@ concussion = as.matrix(concussion)
 out1 = crimCV(apply(concussion[1:3000,10:33],2,as.numeric),2)
 plot(out1)
 
-out_a1 = crimCV(apply(as.matrix(d_a1)[1:300,3:59],2,as.numeric),2)
+out_a1 = crimCV(apply(as.matrix(d_a1)[,3:59],2,as.numeric),2)
 plot(out_a1)
 
-out_a1 = crimCV(apply(as.matrix(d_a1)[1:300,3:59],2,as.numeric),2)
-plot(out_a1)
+out2_a1 = crimCV(apply(as.matrix(d2_a1)[,3:59],2,as.numeric),2)
+plot(out2_a1)
 
 ##Example
 data("TO1adj")
